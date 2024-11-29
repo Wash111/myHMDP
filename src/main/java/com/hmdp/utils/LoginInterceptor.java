a6ee9a6deb08e43d187080c5fbc1c092145abd64
谢浩然 <1316802239@qq.com>
2024-11-29 22:12:37 +0800

    完成了存入Redis数据结构

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,30 +1,45 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 public class LoginInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringRedisTemplate;
+    public LoginInterceptor(StringRedisTemplate stringRedisTemplate){
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
 
     //前置拦截器
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-       //1.获取session
-        HttpSession session = request.getSession();
-        //2.获取session中的用户
-        Object user = session.getAttribute("user");
-        //3.判断用户是否存在
-        if(user == null){
-              //4.不存在，拦截，返回401状态码
-              response.setStatus(401);
-              return false;
+       //1.获取请求头中的token
+        String token = request.getHeader("authorization");
+        if(StrUtil.isBlankIfStr(token)){
+            //拦截，返回401状态码
+            response.setStatus(401);
+            return false;
         }
+        //2基于token获取redis中的用户
+        Map<Object,Object> userMap =  stringRedisTemplate.opsForHash().entries(token);
+        //3.判断用户是否存在
+
+        //将查询到的Hash数据转为UserDTO对象
         //5.存在，保存用户信息到Threadlocal
-        UserHolder.saveUser((UserDTO) user);
+
+        //TODO 刷新TOKEN有效期
+
+
+
         //6.放行
         return true;
     }
